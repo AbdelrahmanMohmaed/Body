c597ddc73ebb2a3f45e0e163a4de9c6da97278c2
AbdelrahmanMohmaed <146894568+AbdelrahmanMohmaed@users.noreply.github.com>
2023-10-04 13:28:08 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,23 @@
+Number = 6
+The_Guessing_Number = int(input("Guess The Number :"))
+Tries = 4
+while The_Guessing_Number != Number:
+    if The_Guessing_Number > Number:
+        print(f"Your number is higher , Correct Number is Lower Than {The_Guessing_Number}")
+        Tries -= 1
+        print(f"You Have only  {Tries} Chances left  ")
+        The_Guessing_Number = int(input("Guess The Number :"))
+        if Tries == 1:
+            print("You Finish Your Chance")
+            break
+    else:
+        print(f"Your Number is Lower,  Correct Number is Higher Than {The_Guessing_Number}")
+        Tries -= 1
+        print(f"You Have only  {Tries} Chances left ")
+        The_Guessing_Number= int(input("Guess The Number"))
+        if Tries == 1:
+            print(" Wrong, You Finish Your Chance")
+            break
+
+else:
+    print(" Correct, Bravo :D")
